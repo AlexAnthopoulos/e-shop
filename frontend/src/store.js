632839801d1037c2750cf9c7ca4bd1e8445b7048
AlexAnthopoulos <alex.anthopoulos01@gmail.com>
2020-10-07 19:40:12 +0200

    started authenticaion and did a few more changes

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -5,7 +5,6 @@ import {
   productListReducer,
   productDetailsReducer,
 } from "./reducers/productReducers";
-
 import { cartReducer } from "./reducers/cartReducers";
 
 const reducer = combineReducers({
@@ -17,10 +16,13 @@ const reducer = combineReducers({
 const cartItemsFromStorage = localStorage.getItem("cartItems")
   ? JSON.parse(localStorage.getItem("cartItems"))
   : [];
+
 const initialState = {
   cart: { cartItems: cartItemsFromStorage },
 };
+
 const middleware = [thunk];
+
 const store = createStore(
   reducer,
   initialState,
