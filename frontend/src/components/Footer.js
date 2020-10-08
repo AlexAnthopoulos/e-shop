61b5eaa52d05aa93249782568bb6edcbc1eff61c
AlexAnthopoulos <alex.anthopoulos01@gmail.com>
2020-10-08 18:34:14 +0200

    final

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -6,9 +6,7 @@ const Footer = () => {
     <footer>
       <Container>
         <Row>
-          <Col className="text-center py-3">
-            Copyright &copy; Alex Anthopoulos
-          </Col>
+          <Col className="text-center py-3">Copyright &copy; Alex</Col>
         </Row>
       </Container>
     </footer>
